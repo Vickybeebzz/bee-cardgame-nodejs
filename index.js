841288830f8d5b86bf978c1c9157f79d9d5d3b47
Vickybeebzz <sr.conejo@icloud.com>
2022-07-15 16:06:14 +0200

    Add UI code

--- index.js
+++ index.js
@@ -49,32 +49,13 @@ class Game {
   onFlip() {}
   onScoreUpdate() {}
 
-  buildMatrix(deck) {
-    matrix.A[1] = deck.cards[0];
-    matrix.A[2] = deck.cards[1];
-    matrix.A[3] = deck.cards[2];
-    matrix.A[4] = deck.cards[3];
-    matrix.B[1] = deck.cards[4];
-    matrix.B[2] = deck.cards[5];
-    matrix.B[3] = deck.cards[6];
-    matrix.B[4] = deck.cards[7];
-    matrix.C[1] = deck.cards[8];
-    matrix.C[2] = deck.cards[9];
-    matrix.C[3] = deck.cards[10];
-    matrix.C[4] = deck.cards[11];
-    matrix.D[1] = deck.cards[12];
-    matrix.D[2] = deck.cards[13];
-    matrix.D[3] = deck.cards[14];
-    matrix.D[4] = deck.cards[15];
-  }
-
   start() {
     if (this.started == true) return;
     this.deck.shuffle();
     for (let i = 0; i < this.deck.cards.length; i++) {
       this.deck.cards[i].id = i;
     }
-    this.buildMatrix(this.deck);
+    buildMatrix(this.deck);
     this.started = true;
     this.flipEnabled = true;
     this.onStart();
@@ -122,31 +103,7 @@ class Game {
   }
 
   updateScore() {
-    console.clear();
-    console.log("   A  B  C  D");
-    console.log(
-      `1 ${matrix.A[1].image} ${matrix.A[2].image} ${matrix.A[3].image} ${matrix.A[4].image}`
-    );
-    console.log(
-      `2 ${matrix.B[1].image} ${matrix.B[2].image} ${matrix.B[3].image} ${matrix.B[4].image}`
-    );
-    console.log(
-      `3 ${matrix.C[1].image} ${matrix.C[2].image} ${matrix.C[3].image} ${matrix.C[4].image}`
-    );
-    console.log(
-      `4 ${matrix.D[1].image} ${matrix.D[2].image} ${matrix.D[3].image} ${matrix.D[4].image}`
-    );
-
-    if (this.score == 8) {
-      console.log("You Win!");
-      this.reset();
-    } else {
-      rl.question("Enter tile (e.g A 2): ", function (tile) {
-        let splitString = tile.split(" ");
-        let id = matrix[splitString[0]][splitString[1]].id;
-        game.flip(id);
-      });
-    }
+    this.onScoreUpdate();
   }
 
   checkCards(card, previousCard) {
@@ -187,6 +144,25 @@ class Deck {
   shuffle = function () {
     shuffle(this.cards);
   };
+}
+
+function buildMatrix(deck) {
+  matrix.A[1] = deck.cards[0];
+  matrix.A[2] = deck.cards[1];
+  matrix.A[3] = deck.cards[2];
+  matrix.A[4] = deck.cards[3];
+  matrix.B[1] = deck.cards[4];
+  matrix.B[2] = deck.cards[5];
+  matrix.B[3] = deck.cards[6];
+  matrix.B[4] = deck.cards[7];
+  matrix.C[1] = deck.cards[8];
+  matrix.C[2] = deck.cards[9];
+  matrix.C[3] = deck.cards[10];
+  matrix.C[4] = deck.cards[11];
+  matrix.D[1] = deck.cards[12];
+  matrix.D[2] = deck.cards[13];
+  matrix.D[3] = deck.cards[14];
+  matrix.D[4] = deck.cards[15];
 }
 
 function shuffle(arr) {
@@ -233,4 +209,31 @@ game.onFlip = (card) => {
   }
 };
 
+game.onScoreUpdate = () => {
+  console.clear();
+  console.log("   A  B  C  D");
+  console.log(
+    `1 ${matrix.A[1].image} ${matrix.A[2].image} ${matrix.A[3].image} ${matrix.A[4].image}`
+  );
+  console.log(
+    `2 ${matrix.B[1].image} ${matrix.B[2].image} ${matrix.B[3].image} ${matrix.B[4].image}`
+  );
+  console.log(
+    `3 ${matrix.C[1].image} ${matrix.C[2].image} ${matrix.C[3].image} ${matrix.C[4].image}`
+  );
+  console.log(
+    `4 ${matrix.D[1].image} ${matrix.D[2].image} ${matrix.D[3].image} ${matrix.D[4].image}`
+  );
+
+  if (game.score == 8) {
+    console.log("You Win!");
+    game.reset();
+  } else {
+    rl.question("Enter tile (e.g A 2): ", function (tile) {
+      let splitString = tile.split(" ");
+      let id = matrix[splitString[0]][splitString[1]].id;
+      game.flip(id);
+    });
+  }
+};
 game.start();
